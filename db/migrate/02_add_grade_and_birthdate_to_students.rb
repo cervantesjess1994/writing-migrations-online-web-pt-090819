95cbace607516db0b65bd6b4b0ffa3c09784f429
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-11-28 07:45:06 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,8 +1,5 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
   def change
-    add_column :students do |t|
-      t.integer :grade
-      t.string :birthdate
-    end
+    add_column :students, :grade, :birthdate
   end
 end
