04f70df2d0bdfd860b83097e6c3dc54556947c65
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-11-28 07:22:38 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++
@@ -1,8 +0,0 @@
-class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
-  def change
-    change_column:students do |t|
-      dir.up (t.change :birthdate, :string)
-      dir.down (t.change :birthdate, :datetime)
-    end
-  end
-end
